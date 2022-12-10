6640a1a2eaedc9f8b6644b1dac7fd864c14fd4e6
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2022-12-10 09:47:12 +0700

    icon gitsign

--- lua/rahman/plugins/lsp/lspconfig.lua
+++ lua/rahman/plugins/lsp/lspconfig.lua
@@ -34,6 +34,14 @@ end
 -- used to enable autocompletion
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+-- Change the Diagnostic symbols in the sign column (gutter)
+-- (not in youtube nvim video)
+local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 lspconfig["html"].setup({
 capabilities = capabilities,
 on_attach = on_attach
