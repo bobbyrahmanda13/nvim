2d22e31eccef576380f449789e528b4ff8da3e11
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-10 13:16:42 +0700

    add symbol to lsp

--- lua/rahman/plugins/lspconfig.lua
+++ lua/rahman/plugins/lspconfig.lua
@@ -3,6 +3,11 @@ local util = require 'lspconfig.util'
 local on_attach = function(_, bufnr)
   local opts = { buffer = bufnr, noremap = true, silent = true }
 
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 -- local borderLsp = "rounded"
 local borderLsp = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" }
