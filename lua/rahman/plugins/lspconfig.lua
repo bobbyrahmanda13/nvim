f399b263b3ae44f2153eb418d342a80e30218562
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-11 21:29:11 +0700

    update lsp keymap

--- lua/rahman/plugins/lspconfig.lua
+++ lua/rahman/plugins/lspconfig.lua
@@ -30,13 +30,16 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
   require('lspconfig.ui.windows').default_options.border = borderLsp
 
   --mapping
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gdc', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gtf', "<cmd>Telescope lsp_definitions<CR>", opts)
+  vim.keymap.set('n', 'gdf', vim.lsp.buf.definition, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
   vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
   vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+  -- vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<leader>vrr', "<cmd>Telescope lsp_references<CR>", opts)
   vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
   vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
   vim.keymap.set('n', '<leader>gf', function()
