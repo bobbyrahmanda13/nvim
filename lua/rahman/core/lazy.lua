35ca6cec7fc6b6da68136e6eb3b7e6c0a365cb90
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-16 11:32:21 +0700

    testing colors

--- lua/rahman/core/lazy.lua
+++ lua/rahman/core/lazy.lua
@@ -16,26 +16,31 @@ local plugins = {
     "catppuccin/nvim", 
     name = "catppuccin", 
     priority = 1000, 
+    lazy = true,
     --		config = function()
     --			vim.cmd.colorscheme("catppuccin")
     --		end
-
   },
+
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
     -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
+
   {
     "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
   },
+
   -- auto completions
   {"hrsh7th/nvim-cmp"},
   {"hrsh7th/cmp-buffer"},
   {"hrsh7th/cmp-path"},
+
   -- configuring lsp servers
   {"neovim/nvim-lspconfig"},
   {"hrsh7th/cmp-nvim-lsp"},
+
   -- snippets
   {"L3MON4D3/LuaSnip"},
   {"saadparwaiz1/cmp_luasnip"},
@@ -46,8 +51,10 @@ local plugins = {
       require('Comment').setup()
     end
   },
+
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 }
+
 local opts = {
   root = vim.fn.stdpath("data") .. "/lazy",
   install = {colorscheme = { "catppuccin" }},
--- lua/rahman/plugins/colors.lua
+++ lua/rahman/plugins/colors.lua
@@ -49,3 +49,4 @@ require("catppuccin").setup({
 
 -- setup must be called before loading
 vim.cmd.colorscheme "catppuccin"
+
