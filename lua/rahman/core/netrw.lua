f62566fec43db9382539f10fee7956d692fe0957
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-24 00:29:04 +0700

    update config netrw

--- lua/rahman/core/netrw.lua
+++ lua/rahman/core/netrw.lua
@@ -1,7 +1,8 @@
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
 vim.g.netrw_keepdir = 0
 vim.g.netrw_winsize = 30
-vim.g.netrw_banner = 0
-vim.g.netrw_localcopydircmd = "cp -r"
+
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
 
 local bind = vim.keymap.set
