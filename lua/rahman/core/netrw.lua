de3ddeb9af984c41cc9e5ae571562cdf5f057714
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-24 06:35:54 +0700

    update netrw

--- lua/rahman/core/netrw.lua
+++ lua/rahman/core/netrw.lua
@@ -1,11 +1,12 @@
+vim.g.netrw_keepdir = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
-vim.g.netrw_keepdir = 0
 vim.g.netrw_winsize = 30
+vim.g.netrw_localcopydircmd = "cp -r"
+vim.cmd([[highlight link netrwMarkFile Search]])
 
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
 
 local bind = vim.keymap.set
 bind("n","<leader>ex",":Lexplore<CR>")
 bind("n","<leader>xx",":Lexplore %:p:h<CR>")
-
