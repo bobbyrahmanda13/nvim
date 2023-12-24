e8fbf3282c36fd0885dc4275739ab8d18ea78d03
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-24 10:59:54 +0700

    update config

--- lua/rahman/plugins/treesitter.lua
+++ lua/rahman/plugins/treesitter.lua
@@ -1,14 +1,13 @@
 require'nvim-treesitter.configs'.setup {
 
-	ensure_installed = { "javascript","typescript","html","css","python","bash","markdown","markdown_inline","lua","sql","regex","json","scss","vue" },
+  ensure_installed = { "javascript","typescript","html","css","python","bash","markdown","markdown_inline","lua","sql","regex","json","scss","vue" },
 
-	sync_install = false,
-	auto_install = true,
-	indent = { enable = true },
-	autotag = { enable = true },
-	highlight = {
-		enable = true,
-
-		additional_vim_regex_highlighting = false,
-	},
+  sync_install = false,
+  auto_install = true,
+  indent = { enable = true },
+  autotag = { enable = true },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 }
