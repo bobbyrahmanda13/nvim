53d26340c25615c9c35019fbcdf472e468890d67
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-24 21:46:14 +0700

    update alacrity padding

--- lua/rahman/plugins/lspconfig.lua
+++ lua/rahman/plugins/lspconfig.lua
@@ -108,59 +108,18 @@ root_dir = vim.loop.cwd
 local tslib_path='/home/rahman/.local/share/pnpm/global/5/node_modules/typescript/lib/'
 
 lspconfig["volar"].setup({
-  capabilities = capabilities,
   on_attach = on_attach,
+  capabilities = capabilities,
   filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+  root_dir = util.root_pattern 'package.json',
   init_options = {
     typescript = {
       tsdk = tslib_path
       -- Alternative location if installed as root:
       -- tsdk = '/usr/local/lib/node_modules/typescript/lib'
     },
-    documentFeatures = {
-      selectionRange = true,
-      linkedEditingRange = true,
-      documentSymbol = true,
-      foldingRange = true,
-      documentFormatting = {
-        defaultPrintWidth = 100,
-        documentFormattingProvider = true,
-        documentRangeFormattingProvider = true,
-        documentOnTypeFormattingProvider = {
-          firstTriggerCharacter = ';',
-          moreTriggerCharacter = '}', '\n',
-        }
-      }
-    }
   }
 })
--- local util = require 'lspconfig.util'
--- local function get_typescript_server_path(root_dir)
---
---   local global_ts = '/home/rahman/.local/share/pnpm/global/5/node_modules/typescript/lib/'
---   -- Alternative location if installed as root:
---   -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
---   local found_ts = ''
---   local function check_dir(path)
---     found_ts =  util.path.join(path, 'node_modules', 'typescript', 'lib')
---     if util.path.exists(found_ts) then
---       return path
---     end
---   end
---   if util.search_ancestors(root_dir, check_dir) then
---     return found_ts
---   else
---     return global_ts
---   end
--- end
---
--- require'lspconfig'.volar.setup{
---   on_attach = on_attach,
---   capabilities = capabilities,
---   on_new_config = function(new_config, new_root_dir)
---     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
---   end,
--- }
 
 -- luasnip setup
 local luasnip = require 'luasnip'
