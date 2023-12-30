ab7529ff51f5077b4160d42a13db4bca98a758a5
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-30 15:34:37 +0700

    update netrw

--- lua/rahman/core/netrw.lua
+++ lua/rahman/core/netrw.lua
@@ -5,11 +5,12 @@ vim.g.netrw_liststyle = 3
 vim.g.netrw_localcopydircmd = "cp -r"
 vim.g.netrw_altv = 1
 
--- Add netrw_gitignore#Hide for Lua files
-vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]] .. ',' .. vim.fn['netrw_gitignore#Hide']() .. ',\\.git$'
-
 -- Set default netrw options
--- local netrwHide = [[\(^\|\s\s\)\zs\.\S\+]]
+vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
+
+-- Add netrw_gitignore#Hide for Lua files
+vim.g.netrw_list_hide = vim.g.netrw_list_hide .. ',' .. vim.fn['netrw_gitignore#Hide']() .. ',\\.git$'
+
 -- vim.g.netrw_list_hide = vim.fn['netrw_gitignore#Hide']() -- default setting
 
 -- vim.g.netrw_browse_split = 0
