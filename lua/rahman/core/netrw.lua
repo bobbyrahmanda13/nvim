6987cd1c8a5521b09b2f8b3fd3cd001df59884a5
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-31 08:16:19 +0700

    update netrw

--- lua/rahman/core/netrw.lua
+++ lua/rahman/core/netrw.lua
@@ -1,10 +1,10 @@
 vim.g.netrw_keepdir = 0
-vim.g.netrw_winsize = 30
+vim.g.netrw_winsize = 20
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_localcopydircmd = "cp -r"
-vim.g.netrw_altv = 1
 vim.g.netrw_chgwin = -1
+-- vim.g.netrw_browse_split = 0
 
 -- Set default netrw options
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
@@ -16,8 +16,9 @@ vim.g.netrw_list_hide = vim.g.netrw_list_hide .. ',' .. vim.fn['netrw_gitignore#
 
 -- vim.g.netrw_list_hide = vim.fn['netrw_gitignore#Hide']() -- default setting
 
--- vim.g.netrw_browse_split = 0
 
 local bind = vim.keymap.set
-bind("n","<leader>ex",":Lexplore<CR>")
-bind("n","<leader>xx",":Lexplore %:p:h<CR>")
+bind("n","<leader>ex",":cd %:p:h<CR>:Lexplore<CR>")
+
+-- bind("n","<leader>ex",":Lexplore<CR>")
+-- bind("n","<leader>xx",":Lexplore %:p:h<CR>")
