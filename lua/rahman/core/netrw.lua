fce720a9caea8dfb32a05970125219f6d2585b98
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-01-28 08:19:50 +0700

    setting netrw

--- lua/rahman/core/netrw.lua
+++ lua/rahman/core/netrw.lua
@@ -1,12 +1,14 @@
 vim.g.netrw_keepdir = 0
-vim.g.netrw_winsize = 30
+vim.g.netrw_winsize = 25
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_localcopydircmd = "cp -r"
 vim.g.netrw_cursor = 1
 vim.g.netrw_keepj = ""
 vim.g.netrw_cygwin = 0
--- vi.g.netrw_browse_split = 0
+-- vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+-- vim.g.netrw_preview = 1
 
 -- Set default netrw options
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
