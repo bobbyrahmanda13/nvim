ca8800f42f30d98f09087512d1a60a2c8f683095
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-04-12 11:01:53 +0700

    fix: change keymap

--- lua/rahman/plugins/lspconfig.lua
+++ lua/rahman/plugins/lspconfig.lua
@@ -81,7 +81,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- bind('n', '<leader>vrr', "<cmd>Telescope lsp_references<CR>", opts)
     bind('n', '<leader>vrn', vim.lsp.buf.rename, opts)
     bind('i', '<C-h>', vim.lsp.buf.signature_help, opts)
-    bind('n', '<leader>f', function()
+    bind('n', '<leader>hf', function()
       vim.lsp.buf.format({ async = true})
     end, opts)
   end,
