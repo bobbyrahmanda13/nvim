706a21fac06be62e9762b8334517e8465748cd7d
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-04-27 22:03:52 +0700

    update setting lsp go

--- lua/rahman/plugins/lspconfig.lua
+++ lua/rahman/plugins/lspconfig.lua
@@ -64,12 +64,9 @@ vim.diagnostic.config({
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
+    local opts = { buffer = ev.buf, silent = true }
     bind('n', 'gdc', vim.lsp.buf.declaration, opts)
     bind('n', 'gdf', vim.lsp.buf.definition, opts)
     bind('n', 'K', vim.lsp.buf.hover, opts)
