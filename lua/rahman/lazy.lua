012d138827c6a1c0aa7411daf1e5611a1c0fda36
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-08-27 20:24:44 +0700

    update lazy.nvim

--- lua/rahman/lazy.lua
+++ lua/rahman/lazy.lua
@@ -1,15 +1,18 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {
@@ -44,4 +47,9 @@ local opts = {
   }
 }
 
-require("lazy").setup({ { import = "rahman.plugins"},{import = "rahman.plugins.lsp"} }, opts )
+require("lazy").setup({ 
+  spec = {
+    { import = "rahman.plugins"},
+    { import = "rahman.plugins.lsp"} },
+  opts
+})
