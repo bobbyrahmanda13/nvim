7b196e170cc249dcb22f417f077ff5bae94af461
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-11-27 07:36:52 +0700

    update rust config lsp

--- lua/rahman/plugins/rustaceanvim.lua
+++ lua/rahman/plugins/rustaceanvim.lua
@@ -6,6 +6,29 @@ return {
     -- Configure rustaceanvim here
     vim.g.rustaceanvim = {
       server = {
+        on_attach = function(client, bufnr)
+
+          local bufnr = vim.api.nvim_get_current_buf()
+          vim.keymap.set(
+            "n", 
+            "<leader>a", 
+            function()
+              vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+              -- or vim.lsp.buf.codeAction() if you don't want grouping.
+            end,
+            { silent = true, buffer = bufnr }
+          )
+          vim.keymap.set(
+            "n", 
+            "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+            function()
+              vim.cmd.RustLsp({'hover', 'actions'})
+            end,
+            { silent = true, buffer = bufnr }
+          )
+
+        end,
+
         default_settings = {
           ['rust-analyzer'] = {
 
