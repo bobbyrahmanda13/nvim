7e997f0447815d162f3e9f4a751da20f5d0f7e58
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-12-04 20:56:50 +0700

    update lazy rtp change to runtimepath

--- lua/rahman/lazy.lua
+++ lua/rahman/lazy.lua
@@ -13,7 +13,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
 local opts = {
   root = vim.fn.stdpath("data") .. "/lazy",
