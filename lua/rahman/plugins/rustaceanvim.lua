937dca86854f8ed66cd584d9ff1a42bb28973a69
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-02-23 19:56:49 +0700

    update and remove keymap rustacean.nvim

--- lua/rahman/plugins/rustaceanvim.lua
+++ lua/rahman/plugins/rustaceanvim.lua
@@ -8,24 +8,24 @@ return {
       server = {
         on_attach = function(client, bufnr)
 
-          local bufnr = vim.api.nvim_get_current_buf()
-          vim.keymap.set(
-            "n", 
-            "<leader>a", 
-            function()
-              vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
-              -- or vim.lsp.buf.codeAction() if you don't want grouping.
-            end,
-            { silent = true, buffer = bufnr }
-          )
-          vim.keymap.set(
-            "n", 
-            "J",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-            function()
-              vim.cmd.RustLsp({'hover', 'actions'})
-            end,
-            { silent = true, buffer = bufnr }
-          )
+          -- local bufnr = vim.api.nvim_get_current_buf()
+          -- vim.keymap.set(
+          --   "n", 
+          --   "<leader>a", 
+          --   function()
+          --     vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+          --     -- or vim.lsp.buf.codeAction() if you don't want grouping.
+          --   end,
+          --   { silent = true, buffer = bufnr }
+          -- )
+          -- vim.keymap.set(
+          --   "n", 
+          --   "J",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+          --   function()
+          --     vim.cmd.RustLsp({'hover', 'actions'})
+          --   end,
+          --   { silent = true, buffer = bufnr }
+          -- )
 
         end,
 
