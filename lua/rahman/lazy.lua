57a56e07fed772b8965d9e5a701fb35ce16b8a22
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-05-29 18:11:38 +0700

    update

--- lua/rahman/lazy.lua
+++ lua/rahman/lazy.lua
@@ -6,20 +6,20 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
   end
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 local opts = {
   root = vim.fn.stdpath("data") .. "/lazy",
   spec = {
-    { import = "rahman.plugins"},
-    { import = "rahman.plugins.lsp"} 
+    { import = "rahman.plugins" },
+    { import = "rahman.plugins.lsp" }
   },
   install = { colorscheme = { "solarized-osaka" } },
   change_detection = {
