5898284ce4675d67e283b089efcd80aa5c20b839
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-07-15 15:42:47 +0700

    update

--- lua/rahman/plugins/lsp/mason.lua
+++ lua/rahman/plugins/lsp/mason.lua
@@ -1,7 +1,7 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
   },
   config = function()
     -- import mason
