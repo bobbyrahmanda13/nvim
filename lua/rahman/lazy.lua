6e18031f077819b62cb04d29fee107ab19bc104f
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-07-15 19:22:02 +0700

    update

--- lua/rahman/lazy.lua
+++ lua/rahman/lazy.lua
@@ -19,7 +19,7 @@ local opts = {
   root = vim.fn.stdpath("data") .. "/lazy",
   spec = {
     { import = "rahman.plugins" },
-    { import = "rahman.plugins.lsp" }
+    { import = "rahman.plugins.lsp" },
   },
   install = { colorscheme = { "solarized-osaka" } },
   change_detection = {
