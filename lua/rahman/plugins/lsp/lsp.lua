01a669fee4d00a9608242736082ebe7da553628f
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-12-07 21:19:34 +0700

    update

--- lua/rahman/plugins/lsp/lsp.lua
+++ lua/rahman/plugins/lsp/lsp.lua
@@ -4,7 +4,6 @@ return {
   -- dependencies = {},
   config = function()
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     vim.lsp.config("*", {
