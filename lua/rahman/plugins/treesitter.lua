aeedf86e8f396d94932f3c70333d2467cfe637ed
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-12-14 09:40:38 +0700

    update

--- lua/rahman/plugins/treesitter.lua
+++ lua/rahman/plugins/treesitter.lua
@@ -2,11 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-
     local configs = require("nvim-treesitter.configs")
     configs.setup({
 
-      ensure_installed = { "javascript","typescript","html","css","python","bash","markdown","markdown_inline","lua","sql","regex","json","scss","zig","vue","go","templ","rust","prisma","graphql","gitignore","c","yaml","toml"},
+      ensure_installed = { "javascript", "typescript", "html", "css", "python", "bash", "markdown", "markdown_inline", "lua", "sql", "regex", "json", "scss", "zig", "vue", "go", "templ", "rust", "prisma", "graphql", "gitignore", "c", "yaml", "toml", "helm", "gotmpl" },
 
       sync_install = false,
       auto_install = true,
