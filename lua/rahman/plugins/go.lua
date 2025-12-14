c74e0ce9997e0163f30e8fe84c2615ab0e8c12eb
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-12-14 09:32:17 +0700

    fix goimports

--- lua/rahman/plugins/go.lua
+++ lua/rahman/plugins/go.lua
@@ -8,6 +8,18 @@ return {
   opts = function()
 
     require("go").setup(opts)
+    -- Run gofmt + goimports on save
+
+    local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require('go.format').goimports()
+      end,
+      group = format_sync_grp,
+    })
+
+    --[[
     local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*.go",
@@ -16,6 +28,9 @@ return {
       end,
       group = format_sync_grp,
     })
+    ]]
+
+    k
     return {
       -- lsp_keymaps = false,
       -- other options
