43065e924d7938fa408033393aa8795e402228c5
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-12-25 07:30:54 +0700

    remove coment format from go nvim config

--- lua/rahman/plugins/go.lua
+++ lua/rahman/plugins/go.lua
@@ -8,28 +8,6 @@ return {
   opts = function()
     require("go").setup()
 
-    -- Run gofmt + goimports on save
-    --[[
-    local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-        require('go.format').goimports()
-      end,
-      group = format_sync_grp,
-    })
-]]
-    --[[
-    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-        require('go.format').goimports()
-      end,
-      group = format_sync_grp,
-    })
-
-]]
     return {
       -- lsp_keymaps = false,
       -- other options
