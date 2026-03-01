fea643a74e196865a76801f81fa265b30f9d8ff0
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-03-01 22:45:26 +0700

    update

--- lua/rahman/plugins/conform.lua
+++ lua/rahman/plugins/conform.lua
@@ -14,7 +14,9 @@ return {
         lsp_format = "fallback",
       },
       -- Set up format-on-save
-      format_on_save = { timeout_ms = 1000 },
+      format_on_save = { 
+        timeout_ms = 1000
+      },
       -- Customize formatters
       formatters = {
         shfmt = {
