53a278da8745d69e7508fc5b420424e58905a36a
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-03-01 18:14:18 +0700

    update

--- lua/rahman/plugins/mason.lua
+++ lua/rahman/plugins/mason.lua
@@ -34,6 +34,7 @@ return {
           "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
       local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
       local vue_plugin = {
         name = '@vue/typescript-plugin',
         location = vue_language_server_path,
