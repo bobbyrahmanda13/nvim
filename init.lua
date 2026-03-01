54050634f9de1a05d1cafd1189b22cc5dd73f94d
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-03-02 06:34:38 +0700

    update

--- init.lua
+++ init.lua
@@ -25,7 +25,7 @@ bind("n", "<leader>to", ":tabnew<CR>")    -- open new tab
 bind("n", "<leader>tx", ":tabclose<CR>")  -- close current tab
 bind("n", "<leader>tn", ":tabn<CR>")      -- go to next tab
 bind("n", "<leader>tp", ":tabp<CR>")      -- go to previous tab
-bind("n", "<leader>tf", ":tabnew %<CR>>") -- go to previous tab
+bind("n", "<leader>tf", ":tabnew %<CR>") -- go to previous tab
 
 bind("n", "<leader>h", "<C-w>h")          -- move cursor window to left
 bind("n", "<leader>j", "<C-w>j")          -- move cursor window to down
