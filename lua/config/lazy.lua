12d2b0859a42cd6db3932c6567f90a73ab705374
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-03-01 20:23:18 +0700

    add new config

---
+++ lua/config/lazy.lua
@@ -0,0 +1,63 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {
+  root = vim.fn.stdpath("data") .. "/lazy",
+  spec = {
+    { import = "rahman.plugins" },
+  },
+  install = { colorscheme = { "solarized-osaka" } },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  ui = {
+    border = "rounded",
+    -- pills = true,
+    icons = {
+      cmd = " ",
+      config = "",
+      event = " ",
+      favorite = " ",
+      ft = " ",
+      init = " ",
+      import = " ",
+      keys = " ",
+      lazy = "󰒲 ",
+      loaded = "●",
+      not_loaded = "○",
+      plugin = " ",
+      runtime = " ",
+      require = "󰢱 ",
+      source = " ",
+      start = " ",
+      task = "✔ ",
+      list = {
+        "●",
+        "➜",
+        "★",
+        "‒",
+      },
+    },
+  }
+}
+
+require("lazy").setup(opts)
