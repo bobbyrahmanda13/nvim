60952a28813dd652af793f6b8b6a23fe57a765e6
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-03-01 18:12:20 +0700

    update

--- lua/rahman/lsp.lua
+++ lua/rahman/lsp.lua
@@ -1,6 +1,5 @@
 local bind = vim.keymap.set
 
-
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
--- lua/rahman/plugins/mason.lua
+++ lua/rahman/plugins/mason.lua
@@ -29,51 +29,49 @@ return {
       }
     },
     "neovim/nvim-lspconfig",
-    -- config = function()
-    -- local mason_registry = require("mason-registry")
-    -- local vue_language_server_path = vim.fn.stdpath('data') ..
-    --     "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-    --
-    -- local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
-    -- local vue_plugin = {
-    --   name = '@vue/typescript-plugin',
-    --   location = vue_language_server_path,
-    --   languages = { 'vue' },
-    --   configNamespace = 'typescript',
-    -- }
-    --
-    -- local vtsls_config = {
-    --   settings = {
-    --     vtsls = {
-    --       tsserver = {
-    --         globalPlugins = {
-    --           vue_plugin,
-    --         },
-    --       },
-    --     },
-    --   },
-    --   filetypes = tsserver_filetypes,
-    -- }
-    --
-    -- local ts_ls_config = {
-    --   init_options = {
-    --     plugins = {
-    --       vue_plugin,
-    --     },
-    --   },
-    --   filetypes = tsserver_filetypes,
-    -- }
-    --
-    -- -- If you are on most recent `nvim-lspconfig`
-    -- local vue_ls_config = {}
-    --
-    -- vim.lsp.config('vtsls', vtsls_config)
-    -- vim.lsp.config('vue_ls', vue_ls_config)
-    -- vim.lsp.config('ts_ls', ts_ls_config)
-    --
-    -- vim.lsp.enable({ 'vtsls', 'vue_ls' }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
-    --
-    -- end
+    config = function()
+      local vue_language_server_path = vim.fn.stdpath('data') ..
+          "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+      local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+      local vue_plugin = {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+        configNamespace = 'typescript',
+      }
+
+      local vtsls_config = {
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                vue_plugin,
+              },
+            },
+          },
+        },
+        filetypes = tsserver_filetypes,
+      }
+
+      local ts_ls_config = {
+        init_options = {
+          plugins = {
+            vue_plugin,
+          },
+        },
+        filetypes = tsserver_filetypes,
+      }
+
+      -- If you are on most recent `nvim-lspconfig`
+      local vue_ls_config = {}
+
+      vim.lsp.config('vtsls', vtsls_config)
+      vim.lsp.config('vue_ls', vue_ls_config)
+      vim.lsp.config('ts_ls', ts_ls_config)
+
+      vim.lsp.enable({ 'vtsls', 'vue_ls' }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
+    end
 
 
   },
