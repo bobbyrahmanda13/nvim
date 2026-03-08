d47732a9d4265b8aa17ba26cad0d1cf7c942ec03
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-03-09 06:10:48 +0700

    update

--- lua/rahman/plugins/nvim_lspconfig.lua
+++ lua/rahman/plugins/nvim_lspconfig.lua
@@ -83,6 +83,7 @@ return {
             },
             staticcheck = true,
             usePlaceholders = false,
+            semanticTokens = true,
             hints = {
               -- Ref: https://github.com/golang/tools/blob/master/gopls/doc/inlayHints.md
               constantValues = true,
