97a0cf460e0b0716cc482cb54d2014e0d3003c28
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-04-03 19:41:03 +0700

    update telescope

--- lua/rahman/plugins/telescope.lua
+++ lua/rahman/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.6',
+  tag = '0.2.2',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
