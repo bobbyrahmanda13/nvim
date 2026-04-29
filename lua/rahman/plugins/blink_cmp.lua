48be90873ffd713ea40a8878449cbe2574bf2822
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-04-29 23:57:28 +0700

    add blink lib

--- lua/rahman/plugins/blink_cmp.lua
+++ lua/rahman/plugins/blink_cmp.lua
@@ -1,7 +1,7 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = { 'rafamadriz/friendly-snippets' ,'saghen/blink.lib' },
     config = function()
       require("blink.cmp").download({force=true,tag = "*"}):wait(60000)
       require("blink.cmp").setup({
